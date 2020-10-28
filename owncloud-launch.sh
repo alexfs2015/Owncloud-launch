333628ca71f0fc1be50468e2fbfc96b28ba3be94
alexfs2015 <44406987+alexfs2015@users.noreply.github.com>
2020-10-28 13:08:22 +0100

    Update owncloud-launch.sh

--- owncloud-launch.sh
+++ owncloud-launch.sh
@@ -2,9 +2,5 @@ OWNCLOUD_FILE=/opt/owncloud-10.5.0-2/ctlscript.sh
 if test -f "$OWNCLOUD_FILE"; then
 	echo '\n#sudo\nalias sudo="sudo "\n\n#owncloud\nalias owncloud-start="sh '$OWNCLOUD_FILE' start"\nalias owncloud-stop="sh '$OWNCLOUD_FILE' stop"' >> ~/.bashrc
 else
-	echo "El archivo no esta en la ubicacion predeterminada"
-	read -p "Introduce la ruta de la carpeta de Owncloud: " ubicacion
-	echo '\n#sudo\nalias sudo="sudo "\n\n#owncloud\nalias owncloud-start="sh '$ubicacion'/ctlscript.sh start"\nalias owncloud-stop="sh '$ubicacion'/ctlscript.sh stop"' >> ~/.bashrc
+	echo "Owncloud tiene que estar en la ruta por defecto"
 fi
-
-source ~/.bashrc
